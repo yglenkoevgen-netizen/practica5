214f3661090a97e7d88f22ff3ca669c5bf174af9
yglenkoevgen-netizen <yglenko.evgen@e-u.edu.ua>
2025-10-05 13:40:16 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const resultInput = document.getElementById('result');
+const buttons = document.querySelector('.buttons');
+
+let currentInput = '0'; // The number currently being typed
+let operator = null;    // The current operator
+let previousInput = null; // The result of the previous operation
